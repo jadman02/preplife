7f55cd922bdcf22e086bb694114f9e35509e1398
jadman02 <jadman02@hotmail.com>
2017-09-07 09:28:40 +1000

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -319,7 +319,7 @@ function payModal(){
 '<div class="pages" style="height:100%;overflow: hidden;">'+
 '<div data-page="buypage" class="page" style="height:100%;overflow: hidden;">'+
 
-      '<div class="newcarddiv" style="position:absolute;bottom:0px;width:100%;z-index:5000000;background-color:#ccc;margin-top:-60px;">'+
+      '<div class="newcarddiv" style="position:absolute;width:100%;z-index:5000000;background-color:#ccc;bottom:60px;">'+
               '  <span class="preloader cards-loader" style="margin:0 auto;margin-top:5px;width:28px;"></span>'+
 
        ' <form action="/charge" method="post" id="payment-form" style="display:none;">'+
