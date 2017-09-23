f5ef9d62d0dc72e4031eb293b629e83ade997c55
jadman02 <jadman02@hotmail.com>
2017-09-24 09:47:54 +1000

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -448,7 +448,7 @@ function payModal(){
 
     '<div id="card-errors" role="alert" style="height:20px;display:block;color:white;"></div>'+
   '</div>'+
-'  <button class="button external active button-big" style="margin:0 auto;width:100%;border-radius:0px;"><i class="pf pf-visa" id="brand-icon"></i>Pay $490.00</button>'+
+'  <button class="button external active button-big" style="margin:0 auto;width:100%;border-radius:0px;"><i class="pf pf-stripe"></i> Pay $490.00</button>'+
 
 '</form>'+
         
