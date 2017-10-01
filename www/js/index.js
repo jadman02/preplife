9eff80dbb81d42c91fda9d74cd8d51b423a777ec
jadman02 <jadman02@hotmail.com>
2017-10-02 08:15:47 +1100

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -561,9 +561,10 @@ $.each(obk, function(i, obj) {
 function chargeSource(){
 alert(globalsource);
     alert(connectedaccount);
-    $.post( "http://www.recountify.com/newcard.php", {uid:f_auth_id,newtoken:'globalsource',amount:100,currency:'AUD',connectedaccount:'connectedaccount'} )
-  .done(function( data ) {  
-            alert(data);
+    alert(f_auth_id);
+    $.post( "http://www.recountify.com/newcard.php", {uid:f_auth_id,newtoken:globalsource,amount:100,currency:'AUD',connectedaccount:connectedaccount} )
+  .done(function(data2) {  
+            alert(data2);
             });
 }
 
